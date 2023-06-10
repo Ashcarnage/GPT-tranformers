56ad94787ff75c7103c3670c5c5c1cec61c5953d
Ashcarnage <86963238+Ashcarnage@users.noreply.github.com>
2023-06-10 23:00:48 +0400

    Add files via upload

--- Neurome/GPT.py
+++ Neurome/GPT.py
@@ -1,16 +1,17 @@
-from tensorflow import keras
+
 import numpy as np 
 
 class layers:
     def __init__(self,inputs,neurons,activation = "relu"):
         self.inputs = np.append(1,np.array(inputs))
         self.weights = np.random.rand(len(self.inputs),neurons)
-    def forward(self):
+    def forward(self,last = False):
         relu = lambda x : np.where(x>=0,x,0.01*x)
-        self.outputs = np.array(relu(np.dot(self.inputs,self.weights)))
+        softmax = lambda x : np.exp(x)/np.sum(np.exp(x))
+        self.outputs = np.array(softmax(np.dot(self.inputs,self.weights))) if last else np.array(relu(np.dot(self.inputs,self.weights)))
         return self.outputs
     def backward(self,output_gradident):
-        relu_dev = lambda x : np.where(x>=0,1,0.01)#1 if x>=0 else 0.01
+        relu_dev = lambda x : np.where(x>=0,1,0.01)
         self.delta_weights = np.dot(self.inputs.T,relu_dev(self.outputs))*output_gradident
         delta_input = np.dot(self.weights[1:,:],relu_dev(self.outputs))*output_gradident
         return np.array(delta_input)
@@ -23,14 +24,15 @@ class Dense:
         self.neurons = neurons
         self.layers = layers
         self.activation = activation
-    def build(self):
+    def build(self,last = False):
         output = self.inputs
         for layer in self.layers:
             if layer!=self.layers-1:
-                synapse = layers(output,self.neurons,self.activation)   
+                synapse = layers(output,self.neurons,self.activation)  
             else:
                 synapse = layers(output,len(output),self.activation)
-            output = synapse.forward() 
+                last = True
+            output = synapse.forward(last) 
             Dense.layer_instance.append(synapse)
         return output
     def feedforward(self):
