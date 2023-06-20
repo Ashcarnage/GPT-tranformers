460b1ea7cbed6fb5703bdd1455578aba89a57596
Ashcarnage <86963238+Ashcarnage@users.noreply.github.com>
2023-06-20 10:28:45 +0400

    Add files via upload

--- Neurome/GPT.py
+++ Neurome/GPT.py
@@ -1,17 +1,20 @@
-
+from tensorflow import keras
 import numpy as np 
 
 class layers:
     def __init__(self,inputs,neurons,activation = "relu"):
         self.inputs = np.append(1,np.array(inputs))
-        self.weights = np.random.rand(len(self.inputs),neurons)
-    def forward(self,last = False):
+        self.neurons = neurons
+        self.weights = np.random.rand(len(self.inputs),self.neurons)
+        self.m = np.zeros([len(self.inputs),self.neurons])
+        self.v = np.copy(self.m)
+
+    def forward(self):
         relu = lambda x : np.where(x>=0,x,0.01*x)
-        softmax = lambda x : np.exp(x)/np.sum(np.exp(x))
-        self.outputs = np.array(softmax(np.dot(self.inputs,self.weights))) if last else np.array(relu(np.dot(self.inputs,self.weights)))
+        self.outputs = np.array(relu(np.dot(self.inputs,self.weights)))
         return self.outputs
     def backward(self,output_gradident):
-        relu_dev = lambda x : np.where(x>=0,1,0.01)
+        relu_dev = lambda x : np.where(x>=0,1,0.01)#1 if x>=0 else 0.01
         self.delta_weights = np.dot(self.inputs.T,relu_dev(self.outputs))*output_gradident
         delta_input = np.dot(self.weights[1:,:],relu_dev(self.outputs))*output_gradident
         return np.array(delta_input)
@@ -24,15 +27,14 @@ class Dense:
         self.neurons = neurons
         self.layers = layers
         self.activation = activation
-    def build(self,last = False):
+    def build(self):
         output = self.inputs
         for layer in self.layers:
             if layer!=self.layers-1:
-                synapse = layers(output,self.neurons,self.activation)  
+                synapse = layers(output,self.neurons,self.activation)   
             else:
                 synapse = layers(output,len(output),self.activation)
-                last = True
-            output = synapse.forward(last) 
+            output = synapse.forward() 
             Dense.layer_instance.append(synapse)
         return output
     def feedforward(self):
@@ -40,12 +42,18 @@ class Dense:
         for layer in Dense.layer_instance:
             layer.inputs = output
             output = layer.forward()
-    def backprop(self):
+    def backprop(self,LR):
         #softmax derivative 
-
-        output_gradient  = 
+        output_gradient  = 12
         for layer in Dense.layer_instance[::-1]:
             output_gradident = layer.backward(output_gradident)
+            del_weights = np.array(layer.delta_weights)
+            layer.m = (0.9*layer.m + 0.1*del_weights)/0.1
+            layer.v = (0.999*layer.v+0.001*del_weights)/0.001
+            layer.weights-=LR*layer.m/(np.sqrt(layer.v)+0.1/10**6)
+
+
+            
 
 
                 
